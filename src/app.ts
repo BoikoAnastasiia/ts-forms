eebf715aa652435e558209670f060dbd7bbd336c
Nastya Boiko <boiko.nastasiia@gmail.com>
2023-09-18 22:34:23 +0200

    added adding input class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElemet: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById('project-input')!
+    );
+    this.hostElemet = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+  private attach() {
+    this.hostElemet.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
